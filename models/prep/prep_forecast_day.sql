923faa2111a7ced1ad95335cb8d13e6288566202
Xiang <tangxiangyi0627@gmail.com>
2024-06-12 15:17:25 +0200

    add features to prep

---
+++ models/prep/prep_forecast_day.sql
@@ -0,0 +1,17 @@
+WITH forecast_day_data AS (
+    SELECT * 
+    FROM {{ref('staging_forecast_day')}}
+),
+add_features AS (
+    SELECT *
+        ,date_part('day',date) AS day_of_month -- day of month as a number
+        ,date_part('month', date) AS month_of_year -- month name as a text
+        ,date_part('year', date) AS year -- year as a number
+        ,date_part('dow',date) AS day_of_week -- weekday name as text
+        ,date_part('week',date) AS week_of_year -- calender week number as number
+        ,to_char(date, 'YYYY-WW') AS year_and_week -- year-calenderweek as text like '2024-43'
+
+    FROM forecast_day_data
+)
+SELECT *
+FROM add_features
