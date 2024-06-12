8c792f989317cdb0a3309d626cb5c9a47c7cd168
Xiang <tangxiangyi0627@gmail.com>
2024-06-12 15:52:17 +0200

    changed weekday format

--- models/prep/prep_forecast_day.sql
+++ models/prep/prep_forecast_day.sql
@@ -7,7 +7,7 @@ add_features AS (
         ,date_part('day',date) AS day_of_month -- day of month as a number
         ,to_char(date,'Month') AS month_of_year -- month name as a text
         ,date_part('year', date) AS year -- year as a number
-        ,to_char(date,'D') AS day_of_week -- weekday name as text
+        ,to_char(date,'Day') AS day_of_week -- weekday name as text
         ,date_part('week',date) AS week_of_year -- calender week number as number
         ,to_char(date, 'YYYY-WW') AS year_and_week -- year-calenderweek as text like '2024-43'
 
--- models/prep/prep_forecast_hour.sql
+++ models/prep/prep_forecast_hour.sql
@@ -7,7 +7,7 @@ add_features AS (
           ,date_time::time AS time -- only time (hours:minutes:seconds) as TIME data type
         ,to_char(date_time,'HH24:MI') as hour -- time (hours:minutes) as TEXT data type
         ,to_char(date,'Month') AS month_of_year -- month name as a text
-        ,to_char(date,'ID') AS day_of_week -- weekday name as text
+        ,to_char(date,'Day') AS day_of_week -- weekday name as text
     FROM forecast_hour_data
 )
 SELECT *
