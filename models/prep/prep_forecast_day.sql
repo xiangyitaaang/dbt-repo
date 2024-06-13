1654c127a5aaf8d0cc3933129992ecff222bfb86
Xiang <tangxiangyi0627@gmail.com>
2024-06-13 15:58:50 +0200

    to_char edit

--- models/prep/prep_forecast_day.sql
+++ models/prep/prep_forecast_day.sql
@@ -5,7 +5,7 @@ WITH forecast_day_data AS (
 add_features AS (
     SELECT *
         ,date_part('day',date) AS day_of_month -- day of month as a number
-        ,to_char(date,'Month') AS month_of_year -- month name as a text
+        ,to_char(date,'FMMonth') AS month_of_year -- month name as a text
         ,date_part('year', date) AS year -- year as a number
         ,to_char(date,'Day') AS day_of_week -- weekday name as text
         ,date_part('week',date) AS week_of_year -- calender week number as number
